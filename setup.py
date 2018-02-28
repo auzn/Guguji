112289de625b358d7519928259ec0af6433a18f0
auzn <auzn.cn@gmail.com>
2018-02-28 12:17:14 +0800

    fix setup.py bug

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(
     name='Guguji',
-    version='0.1.2',
+    version='0.1.4',
     description='A python api for memobird guguji',
+    packages=find_packages(),
+    py_modules=['Guguji'],
     author='auzn',
     author_email='auzn.cn@gmail.com',
     url='https://github.com/auzn/Guguji',
